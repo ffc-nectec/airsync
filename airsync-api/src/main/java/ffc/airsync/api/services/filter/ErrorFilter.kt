5f148cf634d2f5d5c115c3b8a119fc9a1c439cbb
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-05-31 10:36:38 +0700

    iss error mediatype = null

--- airsync-api/src/main/java/ffc/airsync/api/services/filter/ErrorFilter.kt
+++ airsync-api/src/main/java/ffc/airsync/api/services/filter/ErrorFilter.kt
@@ -1,6 +1,7 @@
 package ffc.airsync.api.services.filter
 
 import javax.ws.rs.WebApplicationException
+import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 import javax.ws.rs.ext.ExceptionMapper
 import javax.ws.rs.ext.Provider
@@ -11,7 +12,7 @@ class ErrorFilter : ExceptionMapper<WebApplicationException> {
     override fun toResponse(exception: WebApplicationException?): Response {
         exception!!.printStackTrace()
         val err = ErrorRes(exception.response.status, exception.message, exception)
-        return Response.status(exception.response.statusInfo).entity(err).build()
+        return Response.status(exception.response.statusInfo).entity(err).type(MediaType.APPLICATION_JSON_TYPE).build()
 
     }
 
