64b46ab95834b2a018cecde63dc2152994ac29d1
Piruin Panichphol <piruin.p@gmail.com>
2018-06-29 16:05:09 +0700

    Refactor Main Class

--- airsync-client/src/main/kotlin/ffc/airsync/client/MainContraller.kt
+++ airsync-client/src/main/kotlin/ffc/airsync/client/MainContraller.kt
@@ -21,61 +21,26 @@ import com.google.firebase.auth.FirebaseToken
 import ffc.airsync.client.log.printDebug
 import ffc.airsync.client.module.ApiFactory
 import ffc.airsync.client.module.daojdbi.DatabaseDao
-import ffc.airsync.client.module.daojdbi.JdbiDatabaseDao
 import ffc.airsync.client.webservice.FFCApiClient
 import ffc.airsync.client.webservice.module.FirebaseMessage
 import ffc.entity.Chronic
-import ffc.entity.Link
 import ffc.entity.Organization
 import ffc.entity.Person
-import ffc.entity.System
+import ffc.entity.User
 
-class MainContraller {
+class MainContraller(val org: Organization, val databaseDao: DatabaseDao) {
 
-    companion object {
-        val messageCentral: CentralMessageManage = CentralMessageMaorgUpdatenageV1()
+    fun run() {
+        val org = messageCentral.registerOrganization(org, Config.baseUrlRest)
+        pushData(org)
+        setupNotificationHandlerFor(org)
+        startLocalAirSyncServer()
     }
 
-    fun main(
-        dbHost: String,
-        dbPort: String,
-        dbName: String,
-        dbUsername: String,
-        dbPassword: String,
-        orgId: String,
-        orgName: String,
-        orgCode: String
-    ) {
-
-
-        //get config
-        //check my.ini
-        //check log resume
-        //check database connection
-
-
-        //register central
-        //messageCentral = CentralMessageMaorgUpdatenageV1()
-
-        var org = Organization(orgId).apply {
-            link = Link(System.JHICS, "pcucode" to orgCode)
-            name = orgName
-        }
-        org = messageCentral.registerOrganization(org, Config.baseUrlRest)
-
-        //put user
+    private fun pushData(org: Organization) {
         val userList = ApiFactory().buildUserDao().findAll()
         printDebug("Add put username org = " + org.link!!.keys["pcucode"])
         messageCentral.putUser(userList, org)
-
-
-        //Create connect database
-        val databaseDao: DatabaseDao = JdbiDatabaseDao(
-          dbHost = dbHost,
-          dbPort = dbPort,
-          dbName = dbName,
-          dbUsername = dbUsername,
-          dbPassword = dbPassword)
 
         //put house
         val houseList = databaseDao.getHouse()
@@ -90,19 +55,18 @@ class MainContraller {
         //messageCentral.putChronic(chronicList, org)
 
         val personHaveChronic = insertChronic(personOrgList, chronicList)
-
         messageCentral.putPerson(personHaveChronic, org)
 
         printDebug("Finish push")
+    }
 
-
+    private fun setupNotificationHandlerFor(org: Organization) {
         FirebaseMessage.instant.onUpdateListener = object : FirebaseMessage.OnUpdateListener {
             override fun onUpdate(token: FirebaseToken) {
                 printDebug("OnUpdateListener $token")
                 messageCentral.putFirebaseToken(token, org)
 
             }
-
         }
         FirebaseMessage.instant.onUpdateHouseListener = object : FirebaseMessage.OnUpdateHouseListener {
             override fun onUpdate(_id: String) {
@@ -111,23 +75,27 @@ class MainContraller {
 
             }
         }
+    }
 
+    private fun startLocalAirSyncServer() {
         val ffcApiClient = FFCApiClient("127.0.0.1", 8081)
         ffcApiClient.start()
         ffcApiClient.join()
-
-
     }
 
-
     private fun insertChronic(listPerson: List<Person>, listChronic: List<Chronic>): List<Person> {
-
         listPerson.forEach { person ->
             val chronicList = listChronic.filter { it.link!!.keys["pid"] == person.link!!.keys["pid"] }
-
             chronicList.forEach { person.chronics.add(it) }
         }
-
         return listPerson
+    }
+
+    companion object {
+        val messageCentral: CentralMessageManage = CentralMessageMaorgUpdatenageV1()
+    }
+
+    fun createAirSyncUser(): User = User().update {
+        name = "airsync.jhcis.pcu${org.id}"
     }
 }
