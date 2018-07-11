5fd61b210d1bbdb7d34451603b5bedfac2d14bd6
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-07-11 16:43:21 +0700

    Fix HosDetal

--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryHosDetail.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryHosDetail.kt
@@ -1,5 +1,7 @@
 package ffc.airsync.db
 
+import ffc.airsync.utils.printDebug
+import ffc.entity.gson.toJson
 import org.jdbi.v3.core.mapper.RowMapper
 import org.jdbi.v3.core.statement.StatementContext
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper
@@ -9,30 +11,32 @@ import java.sql.ResultSet
 interface QueryHosDetail {
     @SqlQuery("""
 SELECT
-	office.offid,
-	office.tel,
-	chospital.hosname,
-	cprovince.provname
+	office.offid as offid,
+	office.tel as tel,
+	chospital.hosname as hosname,
+	cprovince.provname as provname
 FROM office
 	LEFT JOIN chospital
 		ON office.offid = chospital.hoscode
 	LEFT JOIN cprovince
-		ON chospital.provcode = cprovince.provcode ;
+		ON chospital.provcode = cprovince.provcode
     """
     )
-    @RegisterRowMapper(HouseDetailMapper::class)
+    @RegisterRowMapper(HosDetailMapper::class)
     fun get(): List<HashMap<String, String>>
 }
 
-class HouseDetailMapper : RowMapper<HashMap<String, String>> {
-    override fun map(rs: ResultSet, ctx: StatementContext): HashMap<String, String> {
-        val detailHos = HashMap<String, String>()
+class HosDetailMapper : RowMapper<HashMap<String, String?>> {
+    override fun map(rs: ResultSet, ctx: StatementContext): HashMap<String, String?> {
+        val detailHos = HashMap<String, String?>()
 
+        rs.next()
         detailHos["hosId"] = rs.getString("offid")
         detailHos["tel"] = rs.getString("tel")
         detailHos["name"] = rs.getString("hosname")
         detailHos["province"] = rs.getString("provname")
 
+        printDebug("DetailHos ${detailHos.toJson()}")
         return detailHos
     }
 }
