37a4a4ca72f98746cc2b42eb9c79fffa19ae3820
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-07-31 10:37:01 +0700

    re-name function

--- impl/airsync-mysql-log-reader/src/main/java/th/in/ffc/airsync/logreader/LogReader.kt
+++ impl/airsync-mysql-log-reader/src/main/java/th/in/ffc/airsync/logreader/LogReader.kt
@@ -10,7 +10,7 @@ import java.util.regex.Pattern
 
 class LogReader(val logfilepath: String, val onLogInput: (line: QueryRecord, tableName: String) -> Unit, val delay: Long = 100) {
 
-    val queryTable = arrayListOf<String>().apply {
+    val startWithBeforeTable = arrayListOf<String>().apply {
         add("INSERT INTO")
         add("UPDATE")
         add("DELETE FROM")
@@ -18,32 +18,32 @@ class LogReader(val logfilepath: String, val onLogInput: (line: QueryRecord, tab
         add("update")
         add("delete from")
     }
-    val selectHouse = arrayListOf<String>()
 
-    private var filters = Arrays.asList<Filters>(
+
+    private var loadFilters = Arrays.asList<Filters>(
             GetTimeFilter(Config.timePattern),
             QueryFilter(Config.logpattern),
             NowFilter(),
             CreateHash()
     )
 
-    private fun processSingle() {
+    private fun readSingleLogFileRealTime() {
         val readLogFile = ReadLogFile(logfilepath, true, delay)
         readLogFile.setListener { record ->
 
-            filters.forEach {
+            loadFilters.forEach {
                 it.process(record)
             }
             if (record.log != "") {
-                onLogInput(record, filterTable(record.log))
+                onLogInput(record, getTableInLogLine(record.log))
             }
         }
         readLogFile.process()
 
     }
 
-    private fun filterTable(logLine: String): String {
-        for (it in queryTable) {
+    private fun getTableInLogLine(logLine: String): String {
+        for (it in startWithBeforeTable) {
             if (logLine.startsWith(it)) {
                 val pattern = Pattern.compile("""^$it `?([\w\d]+)`?.+""")
                 val table = pattern.matcher(logLine)
@@ -55,7 +55,7 @@ class LogReader(val logfilepath: String, val onLogInput: (line: QueryRecord, tab
     }
 
     fun run() {
-        val thread = Thread { processSingle() }
+        val thread = Thread { readSingleLogFileRealTime() }
         thread.start()
     }
 }
