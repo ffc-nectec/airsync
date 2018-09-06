ae7bd31a813ffdfcd771a7b694d6f0e776bf6fa7
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-09-06 14:48:15 +0700

    add TestTime

---
+++ impl/airsync-jhcis-db/src/test/kotlin/DateFunction.kt
@@ -0,0 +1,31 @@
+import ffc.airsync.utils.toTime
+import org.amshove.kluent.`should be equal to`
+import org.joda.time.DateTime
+import org.junit.Test
+import java.sql.Time
+
+class DateFunction {
+    private val dateTime = DateTime(1536218895967)
+    private val time = Time(dateTime.millis)
+
+    @Test
+    fun toTime() {
+        "16:00:59".toTime().toString() `should be equal to` "16:00:59"
+        (time.toTime() < "16:00:00".toTime()) `should be equal to` true
+    }
+
+    @Test
+    fun condition() {
+        ("14:28:15".toTime() < "16:00:00".toTime()) `should be equal to` true
+    }
+
+    @Test
+    fun joda() {
+        dateTime.millis `should be equal to` 1536218895967
+    }
+
+    @Test
+    fun time() {
+        time.toString() `should be equal to` "14:28:15"
+    }
+}
