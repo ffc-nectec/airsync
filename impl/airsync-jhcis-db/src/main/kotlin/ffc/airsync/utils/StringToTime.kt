8d81b84006949338c704baa746cfa4072556950e
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-09-06 14:36:32 +0700

    add utils StringToTime

---
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/utils/StringToTime.kt
@@ -0,0 +1,10 @@
+package ffc.airsync.utils
+
+import org.joda.time.format.DateTimeFormat
+import java.sql.Time
+
+fun String.toTime(): Time {
+    val formatter = DateTimeFormat.forPattern("HH:mm:ss")
+    val dt = formatter.parseDateTime(this)
+    return Time(dt.millis)
+}
