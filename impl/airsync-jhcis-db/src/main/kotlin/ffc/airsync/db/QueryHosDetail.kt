4cd4f08a3acfdf5c24cfa4577b6774d5ce826366
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-09-26 16:05:46 +0700

    change offid to pcucode

--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryHosDetail.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryHosDetail.kt
@@ -32,7 +32,7 @@ class HosDetailMapper : RowMapper<HashMap<String, String?>> {
         val detailHos = HashMap<String, String?>()
 
         rs.next()
-        detailHos["offid"] = rs.getString("offid")
+        detailHos["pcucode"] = rs.getString("offid")
         detailHos["tel"] = rs.getString("tel")
         detailHos["name"] = rs.getString("hosname")
         detailHos["province"] = rs.getString("provname")
