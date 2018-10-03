3666e1b816ff17655f982a717dedecdc29327e64
Piruin Panichphol <piruin.p@gmail.com>
2018-10-03 09:14:21 +0700

    fix ktlint

--- airsync/src/main/kotlin/ffc/airsync/api/retrofit/RetofitApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/retrofit/RetofitApi.kt
@@ -4,9 +4,4 @@ import ffc.entity.House
 import ffc.entity.Organization
 import ffc.entity.Person
 
-abstract class RetofitApi(val persons: List<Person>, val houses: List<House>, val organization: Organization) {
-
-    init {
-
-    }
-}
+abstract class RetofitApi(val persons: List<Person>, val houses: List<House>, val organization: Organization)
