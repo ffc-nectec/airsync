78995b7a91349649dd95d5ccc44be3472ee9cd34
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-10-16 14:20:27 +0700

    add getPosition in Business and FoodShop

--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryFoodShop.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/QueryFoodShop.kt
@@ -3,6 +3,7 @@ package ffc.airsync.db
 import ffc.entity.Link
 import ffc.entity.System
 import ffc.entity.place.Businsess
+import me.piruin.geok.geometry.Point
 import org.jdbi.v3.core.mapper.RowMapper
 import org.jdbi.v3.core.statement.StatementContext
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper
@@ -18,7 +19,9 @@ SELECT
 	villagefoodshop.foodshopno,
 	villagefoodshop.foodshopname as name,
 	villagefoodshop.address,
-	cbusiness.businessdesc as type
+	cbusiness.businessdesc as type,
+	villagefoodshop.xgis,
+	villagefoodshop.ygis
 FROM
 	villagefoodshop
 LEFT JOIN cbusiness ON
@@ -36,6 +39,11 @@ class FoodShopMapper : RowMapper<Businsess> {
             name = rs.getString("name")
             businessType = rs.getString("type")
             no = rs.getString("address")
+
+            val xgis = rs.getDouble("xgis")
+            val ygis = rs.getDouble("ygis")
+            if ((xgis != 0.0) && (ygis != 0.0))
+                location = Point(ygis, xgis)
 
             link = Link(System.JHICS)
             link?.keys?.put("pcucode", rs.getString("pcucode"))
