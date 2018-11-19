384fd6ef3553ebaa1fafb0a43e1e33cd2ccb8e51
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-11-19 16:00:40 +0700

    add QueryVisit

---
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/service/HomeVisitQuery.kt
@@ -0,0 +1,50 @@
+package ffc.airsync.db.service
+
+import ffc.entity.healthcare.CommunityService
+import ffc.entity.healthcare.HomeVisit
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
+import org.jdbi.v3.sqlobject.customizer.Bind
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import org.jdbi.v3.sqlobject.statement.SqlUpdate
+import java.sql.ResultSet
+
+private const val homeHealthQuery = """
+SELECT
+	visithomehealthindividual.homehealthtype,
+	visithomehealthindividual.homehealthdetail,
+	visithomehealthindividual.homehealthresult,
+	visithomehealthindividual.homehealthplan
+FROM
+    visithomehealthindividual
+"""
+
+private const val homehealthIndex = """CREATE  INDEX visitnumber ON visithomehealthindividual(visitno)"""
+
+interface HomeVisitQuery {
+    @SqlUpdate(homehealthIndex)
+    fun createIndex()
+
+    @SqlQuery(
+        homeHealthQuery + """
+WHERE visithomehealthindividual.visitno = :visitnumber
+    """
+    )
+    fun get(@Bind("visitnumber") visitnumber: Int): List<HomeVisit>
+}
+
+class VisitHomeHealthMapper : RowMapper<HomeVisit> {
+    override fun map(rs: ResultSet, ctx: StatementContext?): HomeVisit {
+        return HomeVisit(
+            serviceType = CommunityService.ServiceType(
+                id = rs.getString("homehealthtype"),
+                name = ""
+            )
+        ).apply {
+            rs.getString("homehealthdetail")?.let { detail = it }
+            rs.getString("homehealthresult")?.let { result = it }
+            rs.getString("homehealthplan")?.let { plan = it }
+        }
+    }
+}
+
---
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/db/service/SpecialppQuery.kt
@@ -0,0 +1,33 @@
+package ffc.airsync.db.service
+
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper
+import org.jdbi.v3.sqlobject.customizer.Bind
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import java.sql.ResultSet
+
+private const val specialQuery = """
+SELECT
+	f43specialpp.ppspecial
+FROM
+	f43specialpp
+"""
+
+private const val visitNumberIndex = """CREATE  INDEX visitnumber ON f43specialpp(visitno)"""
+
+interface SpecialppQuery {
+    @SqlQuery(
+        specialQuery + """
+    WHERE f43specialpp.visitno = :visitnumber
+    """
+    )
+    @RegisterRowMapper(SpecialPPMapper::class)
+    fun get(@Bind("visitnumber") visitnumber: Int): List<String>
+}
+
+class SpecialPPMapper : RowMapper<String> {
+    override fun map(rs: ResultSet, ctx: StatementContext?): String {
+        return rs.getString("ppspecial")
+    }
+}
