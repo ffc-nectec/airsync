b460f4e9bcb6b3c4b56ecc41c9bbd9c1a2c64c4c
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-01-08 16:26:03 +0700

    airsync run with jPortable64

--- FFC.bat
+++ FFC.bat
@@ -1,1 +1,1 @@
-"%JAVA_HOME%\bin\java.exe" -d64 -Xms1G -Xmx2G -jar -Dfile.encoding=UTF-8  %~dp0airsync.jar%*
+JavaCommonFiles\Java64\bin\java.exe -d64 -Xms1G -Xmx2G -jar -Dfile.encoding=UTF-8  airsync.jar
