d353228160418c6fce5594077c68c5e8abf3bbd6
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-26 13:20:48 +0700

    add CheckLauncherVersion

---
+++ airsync/src/main/kotlin/ffc/airsync/CheckLauncherVersion.kt
@@ -0,0 +1,43 @@
+package ffc.airsync
+
+import max.download.zip.ZIpDownload
+import max.githubapi.GitHubLatestApi
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileReader
+import java.net.URL
+
+class CheckLauncherVersion {
+    fun check() {
+        printDebug("Check Launcher Version")
+        var launcherVersion = ""
+        try {
+            val fr = FileReader("launcher.version")
+            launcherVersion = fr.readText().trim()
+            fr.close()
+        } catch (ignore: FileNotFoundException) {
+        }
+
+        val gh = GitHubLatestApi("ffc-nectec/AirSyncLauncher").getLastRelease()
+        printDebug("Check launcher local version $launcherVersion and git version ${gh.tag_name}")
+        if (gh.tag_name != launcherVersion) {
+            val ass = gh.assets.find { it.name == "install.zip" }
+            val downloadUrl = ass?.browser_download_url
+            if (downloadUrl != null) {
+                println("Launcher download...")
+                val zipD = ZIpDownload(URL(downloadUrl)) {
+                    printDebug("Launcher download ${((it / ass.size) * 100)} %")
+                }
+                zipD.download(File(""))
+                try {
+                    printDebug("Start Launcher...")
+                    Runtime.getRuntime().exec("cmd /k start AirSyncLauncher.exe")
+                    Thread.sleep(5000)
+                } catch (ex: Exception) {
+                    printDebug("Cannot run launcher ${ex.message}")
+                }
+            }
+            System.exit(1)
+        }
+    }
+}
