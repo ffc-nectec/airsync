f0b8e012409dee4d4bb7e5161e169c868c68a14f
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-03-21 15:40:32 +0700

    add track 401 auth cloud.

--- airsync/src/main/kotlin/ffc/airsync/api/template/RetofitTemplateApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/template/RetofitTemplateApi.kt
@@ -3,15 +3,27 @@ package ffc.airsync.api.template
 import ffc.airsync.retrofit.RetofitApi
 import ffc.airsync.utils.callApi
 import ffc.entity.Template
+import javax.ws.rs.NotAuthorizedException
 
 class RetofitTemplateApi : RetofitApi<TemplateUrl>(TemplateUrl::class.java), TemplateApi {
     override fun clearAndCreate(template: List<Template>) {
         callApi {
-            restService.clearnAndCreate(
+            val response = restService.clearnAndCreate(
                 orgId = organization.id,
                 authkey = tokenBarer,
                 template = template
             ).execute()
+
+            if (response.code() != 201)
+                if (response.code() == 401) {
+                    var errorBody = ""
+                    response.errorBody()?.byteStream()?.reader()?.readLines()?.let { error ->
+                        error.forEach {
+                            errorBody += it + "\r\n"
+                        }
+                    }
+                    throw NotAuthorizedException("Create Template code ${response.code()} $errorBody")
+                }
         }
     }
 }
