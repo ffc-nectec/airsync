8fec77f8a90341f6049ceea4fd448dfef91d7d38
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-03-26 15:43:49 +0700

    read config from JHCIS database config.

---
+++ airsync/src/main/kotlin/ffc/airsync/JhcisConfig.kt
@@ -0,0 +1,22 @@
+package ffc.airsync
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+
+class JhcisConfig {
+    private val jhcisConfigFile = "C:\\Program Files\\JHCIS\\database.properties"
+    private val property = Properties()
+
+    init {
+        val file = File(jhcisConfigFile)
+        require(file.isFile) { "ไม่พบไฟล์ database.properties ของตัวระบบ JHCIS fail load $jhcisConfigFile" }
+        property.load(FileInputStream(file))
+    }
+
+    val server get() = property.getProperty("SERVER")
+    val port get() = property.getProperty("PORT")
+    val username get() = property.getProperty("USERNAME")
+    val password get() = property.getProperty("PASSWORD")
+    val databaseName get() = property.getProperty("DATABASE")
+}
