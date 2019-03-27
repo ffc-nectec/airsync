0e421d267cad40a2f7e0b1a148a4d670d564f454
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-03-27 10:40:37 +0700

    Add get mysql base dir from mysql query.

---
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/mysqlvariable/QueryMySqlVariable.kt
@@ -0,0 +1,25 @@
+package ffc.airsync.mysqlvariable
+
+import org.jdbi.v3.core.mapper.RowMapper
+import org.jdbi.v3.core.statement.StatementContext
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper
+import org.jdbi.v3.sqlobject.statement.SqlQuery
+import java.sql.ResultSet
+
+interface QueryMySqlVariable {
+    @SqlQuery("SHOW VARIABLES LIKE 'basedir'")
+    @RegisterRowMapper(VariableMapping::class)
+    fun getBaseDir(): List<String>
+
+    @SqlQuery("SHOW VARIABLES LIKE 'datadir'")
+    @RegisterRowMapper(VariableMapping::class)
+    fun getDataDir(): List<String>
+}
+
+internal class VariableMapping : RowMapper<String> {
+    override fun map(rs: ResultSet, ctx: StatementContext): String {
+        return rs.getString("Value")
+    }
+}
+
+
