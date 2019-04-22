20af15751f8ce264029a24a99108b58f6c61e303
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-04-22 15:10:23 +0700

    reflactor GUI code

--- impl/airsync-gui-swing/src/main/kotlin/ffc/airsync/gui/drawable/util.kt
+++ impl/airsync-gui-swing/src/main/kotlin/ffc/airsync/gui/drawable/util.kt
@@ -1,3 +1,28 @@
 package ffc.airsync.gui.drawable
 
-fun String.getFileResource() = Thread.currentThread().contextClassLoader.getResourceAsStream(this)
+import java.awt.Font
+import java.awt.GraphicsEnvironment
+import java.awt.Image
+import java.awt.Toolkit
+import javax.imageio.ImageIO
+
+internal fun String.getFileResource() = Thread.currentThread().contextClassLoader.getResourceAsStream(this)
+
+internal fun getScreenSize(): Pair<Int, Int> {
+    val gd = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+    return Pair(gd.displayMode.width, gd.displayMode.height)
+}
+
+internal fun String.getImageResource(): Image {
+    val resourceStream = getFileResource()
+    val bufferImageIO = ImageIO.read(resourceStream)
+    return Toolkit.getDefaultToolkit().createImage(bufferImageIO.source)
+}
+
+internal fun String.getImageScalingResource(width: Int, height: Int): Image {
+    val image = this.getImageResource()
+    return image.getScaledInstance(width, height, Image.SCALE_SMOOTH)
+}
+
+internal val kanitBold = Font.createFont(Font.TRUETYPE_FONT, "font/Kanit-Bold.otf".getFileResource())
+internal val kanitMedium = Font.createFont(Font.TRUETYPE_FONT, "font/Kanit-Medium.otf".getFileResource())
