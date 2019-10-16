e0e3d9dbc00799216fa31caaa83829d151e79ddb
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-10-16 14:22:43 +0700

    rename local to jhcis

--- airsync/src/main/kotlin/ffc/airsync/api/user/User.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/user/User.kt
@@ -21,8 +21,9 @@ fun ArrayList<User>.initSync() {
     val localUser = arrayListOf<User>()
     localUser.addAll(localUser.load())
 
+    val jhcisUser = User().gets()
     if (localUser.isEmpty()) {
-        localUser.addAll(User().gets())
+        localUser.addAll(jhcisUser)
         check(localUser.isNotEmpty()) {
             "เกิดข้อผิดพลาด " +
                     "ในการดึงข้อมูลการ Login ใน table user " +
@@ -36,17 +37,15 @@ fun ArrayList<User>.initSync() {
         }
         save()
     } else {
-        checkAndUpdateNewUser(localUser)
+        val cloudUser = userApi.getuser()
+        checkDataUpdate(jhcisUser, cloudUser, { jhcis, cloud -> jhcis.name == cloud.name }) {
+            getLogger(this).info { "Update new user ${it.toJson()}" }
+            val putUser = userApi.putUser(it.toMutableList())
+            localUser.addAll(putUser)
+            save()
+        }
         addAll(localUser)
     }
 }
 
-private fun ArrayList<User>.checkAndUpdateNewUser(localUser: ArrayList<User>) {
-    val cloudUser = userApi.getuser()
-    checkDataUpdate(localUser, cloudUser, { local, cloud -> local.name == cloud.name }) {
-        getLogger(this).info { "Update new user ${it.toJson()}" }
-        val putUser = userApi.putUser(it.toMutableList())
-        localUser.addAll(putUser)
-        save()
-    }
-}
+
--- airsync/src/main/kotlin/ffc/airsync/utils/checkDataUpdate.kt
+++ airsync/src/main/kotlin/ffc/airsync/utils/checkDataUpdate.kt
@@ -2,19 +2,19 @@ package ffc.airsync.utils
 
 /**
  * ใช้สำหรับช่วยในการสร้างชุดคำสั่ง สำหรับการตรวจสอบการ update ข้อมูลใหม่ขึ้นไป cloud
- * @param localData ข้อมูลที่อยู่บน local ในที่นี้จะเป็นข้อมูลจาก jhcis
+ * @param jhcisData ข้อมูลที่อยู่บน local ในที่นี้จะเป็นข้อมูลจาก jhcis
  * @param cloudData ข้อมูลที่อยู่บน cloud
  * @param checkItem กฏการตรวจสอบว่า วัตถุ local และ cloud เหมือนกัน
  * @param updateNewData ถ้าเกิดมีข้อมูลใหม่จะ update จะให้ทำอย่างไร
  */
 fun <T> checkDataUpdate(
-    localData: List<T>,
+    jhcisData: List<T>,
     cloudData: List<T>,
-    checkItem: (local: T, cloud: T) -> Boolean,
+    checkItem: (jhcis: T, cloud: T) -> Boolean,
     updateNewData: (newData: List<T>) -> Unit
 ) {
     val newData = arrayListOf<T>()
-    localData.forEach { local ->
+    jhcisData.forEach { local ->
         val cloud = cloudData.find { checkItem(local, it) }
         if (cloud == null)
             newData.add(local)
