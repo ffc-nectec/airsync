fc640c19782871e8edb2e1ff63850dd6f65fd3b0
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-10-18 10:52:37 +0700

    clean

--- airsync/src/main/kotlin/ffc/airsync/utils/syncCloudApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/utils/syncCloudApi.kt
@@ -2,12 +2,5 @@ package ffc.airsync.utils
 
 import ffc.airsync.api.autosync.RetofitSyncCloud
 import ffc.airsync.api.autosync.SyncCloud
-import ffc.airsync.gui
-import ffc.airsync.ui.AirSyncGUI.MESSAGE_TYPE.INFO
-import ffc.airsync.ui.createMessage
 
-val syncCloud: SyncCloud by lazy {
-    RetofitSyncCloud {
-        gui.createMessage("SYNC_CLOUD", it, INFO)
-    }
-}
+val syncCloud: SyncCloud by lazy { RetofitSyncCloud() }
