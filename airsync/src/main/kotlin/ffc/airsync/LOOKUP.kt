41807d75a4201583582787964746b333d615deae
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-11-01 11:19:08 +0700

    แก้ไขเวลา visit แล้ว ค้นหา user หรือ person ไม่เจอ ให้ข้ามข้อมูลนี้ไป จะเป็นตอนที่เกิดตอนกวาดข้อมูลเก่า แล้วชื่อ user ใน visit เป็นคนละชื่อกับ user เพราะว่า jhcis จะบันทึกเป็นชื่อเลย ไม่ได้ map เอา

--- airsync/src/main/kotlin/ffc/airsync/LOOKUP.kt
+++ airsync/src/main/kotlin/ffc/airsync/LOOKUP.kt
@@ -3,8 +3,20 @@ package ffc.airsync
 import ffc.airsync.api.person.findPersonId
 import ffc.airsync.api.user.findProviderId
 
-val lookupPersonId = { pid: String -> findPersonId(pid) }
-val lookupUserId = { name: String -> findProviderId(name) }
+val lookupPersonId = { pid: String ->
+    try {
+        findPersonId(pid)
+    } catch (ex: KotlinNullPointerException) {
+        ""
+    }
+}
+val lookupUserId = { name: String ->
+    try {
+        findProviderId(name)
+    } catch (ex: KotlinNullPointerException) {
+        ""
+    }
+}
 val lookupDisease = { icd10: String -> icd10Api.lookup(icd10) }
 val lookupServiceType = { serviceId: String -> homeHealthTypeApi.lookup(serviceId) }
 val lookupSpecialPP = { ppCode: String -> specialPpApi.lookup(ppCode.trim()) }
