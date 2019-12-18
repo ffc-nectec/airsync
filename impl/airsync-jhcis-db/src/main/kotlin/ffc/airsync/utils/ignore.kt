005a5342d347f148546a1aeeeb01a847179fb677
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-12-18 23:00:48 +0700

    fix bug get bloodPressureD2 error

--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/utils/ignore.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/utils/ignore.kt
@@ -1,6 +1,6 @@
 package ffc.airsync.utils
 
-inline fun <reified T> ignore(callback: () -> T): T? {
+inline fun <reified T> ignore(callback: () -> T?): T? {
     return try {
         callback()
     } catch (ignore: Exception) {
