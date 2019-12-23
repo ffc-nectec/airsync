277df36b9fdf25e3711983a04bd39475db74f148
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-12-23 14:22:15 +0700

    add uninstall.bat

---
+++ uninstall.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Uninstall FFC >> "%FFC_HOME%\uninstall.log"
+echo Delete launcher.version >> uninstall.log
+DEL /S /Q "%FFC_HOME%\launcher.version"
+
+echo Delete data >> "%FFC_HOME%\uninstall.log"
+RD /S /Q "%FFC_HOME%\data"
+
+echo Delete jreVersion.txt >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\jreVersion.txt"
+
+echo Delete log.cfg >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\log.cfg"
+
+echo Delete airsync.jar >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\airsync.jar"
+
+echo Delete jre >> "%FFC_HOME%\uninstall.log"
+RD /S /Q "%FFC_HOME%\jre"
+
+echo Delete ffc-airsync.exe >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\ffc-airsync.exe"
+
+echo Delete uninstall.ffc >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\uninstall.ffc"
+
+echo Delete  uninstall.bat >> "%FFC_HOME%\uninstall.log"
+DEL /S /Q "%FFC_HOME%\uninstall.bat"
+
+echo Remove FFC_HOME >> "%FFC_HOME%\uninstall.log"
+setx FFC_HOME ""
+REG delete HKCU\Environment /F /V FFC_HOME
+exit
