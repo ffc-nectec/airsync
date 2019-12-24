d64626c39fcbf4bbb36d8dc00d6d9a906d9ce267
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-12-24 10:46:01 +0700

    update uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,40 +1,39 @@
-@echo off
-echo Uninstall FFC >> "%FFC_HOME%\uninstall.log"
-echo Delete launcher.version >> uninstall.log
+@echo on
+echo Uninstall FFC
+echo Delete launcher.version
 DEL /S /Q "%FFC_HOME%\launcher.version"
 
-echo Delete data >> "%FFC_HOME%\uninstall.log"
+echo Delete data
 RD /S /Q "%FFC_HOME%\data"
 
-echo Delete jreVersion.txt >> "%FFC_HOME%\uninstall.log"
+echo Delete jreVersion.txt
 DEL /S /Q "%FFC_HOME%\jreVersion.txt"
 
-echo Delete log.cfg >> "%FFC_HOME%\uninstall.log"
+echo Delete log.cfg
 DEL /S /Q "%FFC_HOME%\log.cfg"
 
-echo Delete FFC startup >> "%FFC_HOME%\uninstall.log"
+echo Delete FFC startup
 DEL /S /Q "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ffc-airsync.bat"
 
-echo Delete airsync.jar >> "%FFC_HOME%\uninstall.log"
+echo Delete airsync.jar
 DEL /S /Q "%FFC_HOME%\airsync.jar"
 
-echo Delete jre >> "%FFC_HOME%\uninstall.log"
+echo Delete jre
 RD /S /Q "%FFC_HOME%\jre"
 
-echo Delete ffc-airsync.exe >> "%FFC_HOME%\uninstall.log"
+echo Delete ffc-airsync.exe
 DEL /S /Q "%FFC_HOME%\ffc-airsync.exe"
 
-echo Delete uninstall.ffc >> "%FFC_HOME%\uninstall.log"
+echo Delete uninstall.ffc
 DEL /S /Q "%FFC_HOME%\uninstall.ffc"
 
-echo Delete  uninstall.bat >> "%FFC_HOME%\uninstall.log"
+echo Delete  uninstall.bat
 DEL /S /Q "%FFC_HOME%\uninstall.bat"
 
-echo Remove FFC_HOME >> "%FFC_HOME%\uninstall.log"
+echo Remove FFC_HOME
 setx FFC_HOME ""
 REG delete HKCU\Environment /F /V FFC_HOME
 
 echo Success uninstall FFC Airsync
 
-SLEEP 10
 exit
