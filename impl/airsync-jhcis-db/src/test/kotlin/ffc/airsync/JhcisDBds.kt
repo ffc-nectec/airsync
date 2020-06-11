fbc8d4f15df6b2276474f934507a1cbddb1172a0
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-06-11 08:50:56 +0700

    เพิ่มตัวต่อ mysql สำหรับทดสอบ code

---
+++ impl/airsync-jhcis-db/src/test/kotlin/ffc/airsync/JhcisDBds.kt
@@ -0,0 +1,33 @@
+package ffc.airsync
+
+import javax.sql.DataSource
+
+class JhcisDBds {
+
+    fun get(): DataSource {
+        Class.forName("com.mysql.jdbc.Driver")
+
+        val dsMySql = com.mysql.jdbc.jdbc2.optional.MysqlDataSource()
+
+        val dbHost = "127.0.0.1"
+        val dbPort = "3333"
+        val dbName = "jhcisdb"
+        val dbUsername = "root"
+        val dbPassword = "123456"
+
+        dsMySql.setURL(
+            "jdbc:mysql://$dbHost:$dbPort/$dbName?" +
+                    "autoReconnect=true&" +
+                    "useSSL=false&" +
+                    "maxReconnects=2&" +
+                    "autoReconnectForPools=true&" +
+                    "connectTimeout=10000&" +
+                    "socketTimeout=10000"
+        )
+        dsMySql.databaseName = dbName
+        dsMySql.user = dbUsername
+        dsMySql.setPassword(dbPassword)
+        dsMySql.port = dbPort.toInt()
+        return dsMySql
+    }
+}
