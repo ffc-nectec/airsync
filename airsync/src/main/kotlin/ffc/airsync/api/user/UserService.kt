e00ab29f9866cb40bf841ecb75cb82a5b3b097be
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-06-30 09:46:25 +0700

    เพิ่มส่วน api user - ลบ user - อัพเดท user

--- airsync/src/main/kotlin/ffc/airsync/api/user/UserService.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/user/UserService.kt
@@ -4,9 +4,11 @@ import ffc.airsync.APIVERSION
 import ffc.entity.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface UserService {
@@ -21,5 +23,19 @@ interface UserService {
     fun getUser(
         @Path("orgId") orgId: String,
         @Header("Authorization") authkey: String
+    ): Call<List<User>>
+
+    @DELETE("/$APIVERSION/org/{orgId}/user")
+    fun deleteUser(
+        @Path("orgId") orgId: String,
+        @Header("Authorization") authkey: String,
+        @Body user: List<String>
+    ): Call<Map<String, Boolean>>
+
+    @PUT("/$APIVERSION/org/{orgId}/user")
+    fun updateUser(
+        @Path("orgId") orgId: String,
+        @Header("Authorization") authkey: String,
+        @Body user: List<User>
     ): Call<List<User>>
 }
