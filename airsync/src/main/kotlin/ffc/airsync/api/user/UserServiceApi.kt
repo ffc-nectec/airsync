3d528b339a51fc022fa384bf1e9b19266f91c04a
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-07-08 16:46:33 +0700

    เพิ่ม user อสม.

--- airsync/src/main/kotlin/ffc/airsync/api/user/UserServiceApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/user/UserServiceApi.kt
@@ -3,6 +3,7 @@ package ffc.airsync.api.user
 import ffc.airsync.retrofit.RetofitApi
 import ffc.airsync.utils.callApi
 import ffc.entity.User
+import ffc.entity.copy
 
 class UserServiceApi : RetofitApi<UserService>(UserService::class.java), UserApi {
     override fun create(userInfoList: List<User>): List<User> {
@@ -32,11 +33,16 @@ class UserServiceApi : RetofitApi<UserService>(UserService::class.java), UserApi
     }
 
     override fun update(update: List<User>): List<User> {
+        val list = update.map {
+            val copy = it.copy()
+            copy.link?.keys?.set("password", it.password)
+            copy
+        }
         return callApi {
-            val execute = restService.updateUser(organization.id, tokenBarer, update).execute()
+            val execute = restService.updateUser(organization.id, tokenBarer, list).execute()
             when (val status = execute.code()) {
                 200 -> execute.body()
-                else -> throw Exception("Api error $status ${execute.errorBody()}")
+                else -> throw Exception("Api error $status ${execute.errorBody()?.charStream()?.readText()}")
             }
         }
     }
--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/user/UserJdbi.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/user/UserJdbi.kt
@@ -3,12 +3,19 @@ package ffc.airsync.user
 import ffc.airsync.Dao
 import ffc.airsync.MySqlJdbi
 import ffc.airsync.extension
+import ffc.airsync.getLogger
 import ffc.entity.User
 
 class UserJdbi(
     val jdbiDao: Dao = MySqlJdbi(null)
 ) : UserDao {
+    private val logger = getLogger(this)
     override fun get(): List<User> {
-        return jdbiDao.extension<QueryUser, List<User>> { get() }
+        return jdbiDao.extension<QueryUser, List<User>> { get() }.filter {
+            kotlin.runCatching {
+                val test = "${it.name}:" + it.password
+                logger.debug("User test password $test")
+            }.isSuccess
+        }
     }
 }
