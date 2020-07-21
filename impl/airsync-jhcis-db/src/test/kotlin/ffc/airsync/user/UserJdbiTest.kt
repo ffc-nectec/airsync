49af085503e1ace4110a596667c754eaaa1b1fda
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-07-21 16:29:59 +0700

    เพิ่ม process ตรวจสอบ อสม. ดูแลบ้าน

--- impl/airsync-jhcis-db/src/test/kotlin/ffc/airsync/user/UserJdbiTest.kt
+++ impl/airsync-jhcis-db/src/test/kotlin/ffc/airsync/user/UserJdbiTest.kt
@@ -1,7 +1,7 @@
 package ffc.airsync.user
 
-import ffc.airsync.JhcisDBds
-import ffc.airsync.MySqlJdbi
+import ch.vorburger.mariadb4j.DB
+import ffc.entity.User
 import org.junit.Ignore
 import org.junit.Test
 
@@ -10,11 +10,25 @@ import org.junit.Test
  */
 @Ignore("สำหรับทดสอบ")
 class UserJdbiTest {
-    val dao = UserJdbi(MySqlJdbi(JhcisDBds().get()))
+    // val dao = UserJdbi(MySqlJdbi(JhcisDBds().get()))
 
     @Test
     fun get() {
-        val data = dao.get()
-        println(data)
+        /*val data = dao.get()
+        data.forEach { user ->
+            println("${user.getIdCard()}")
+        }
+        println(data)*/
     }
+
+    @Test
+    fun startDb() {
+        val database = DB.newEmbeddedDB(34323)
+        println("create")
+        // database.start()
+    }
+}
+
+internal fun User.getIdCard(): String? {
+    return this.link?.keys?.get("idcard")?.toString()
 }
