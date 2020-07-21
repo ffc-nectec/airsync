d261c545db5fabdd69c10f9583077a0a350ab46b
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-07-21 16:48:26 +0700

    เพิ่ม user อสม. พร้อมใช้กับ api

--- airsync/src/main/kotlin/ffc/airsync/api/house/HouseApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/house/HouseApi.kt
@@ -6,5 +6,5 @@ import ffc.entity.place.House
 interface HouseApi {
     fun putHouse(houseList: List<House>, progressCallback: (Int) -> Unit, clearCloud: Boolean = true): List<House>
     fun syncHouseFromCloud(_id: String, databaseDao: DatabaseDao)
-    fun syncHouseToCloud(house: House)
+    fun syncHouseToCloud(house: House): House
 }
