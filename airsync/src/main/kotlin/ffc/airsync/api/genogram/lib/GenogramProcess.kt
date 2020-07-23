869489217d45e5607ead238c7040bbc008802019
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-07-24 05:49:20 +0700

    done genolib

--- airsync/src/main/kotlin/ffc/airsync/api/genogram/lib/GenogramProcess.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/genogram/lib/GenogramProcess.kt
@@ -9,5 +9,5 @@ interface GenogramProcess<P> {
      * @param dataFunction ฟังชั่นสำหรับดึงข้อมูลคนที่จำเป็นสำหรับการจับคู่ Genogram
      * @return คนที่ใส่ความสัมพันธุ์แล้ว
      */
-    fun process(persons: List<P>): List<P>
+    fun process(persons: List<P>)
 }
