52fa5965555e6fecaaae3097e956efa4a12a76cd
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-08-06 08:12:41 +0700

    fix query sugarLab Error

--- impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/visit/SugarLabQuery.kt
+++ impl/airsync-jhcis-db/src/main/kotlin/ffc/airsync/visit/SugarLabQuery.kt
@@ -3,6 +3,8 @@ package ffc.airsync.visit
 import ffc.airsync.Dao
 import ffc.airsync.MySqlJdbi
 import ffc.airsync.getLogger
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 internal class SugarLabQuery(private val jdbiDao: Dao = MySqlJdbi(null)) {
     private val logger = getLogger(this)
@@ -17,6 +19,20 @@ AND visitlabsugarblood.visitno = :visitnumber
     """
 
     fun get(pcuCode: String, visitNumber: Int): Double? {
+        var i = 0
+        while (true) {
+            try {
+                return getSugar(pcuCode, visitNumber)
+            } catch (ex: Exception) {
+                if (i > 5) throw ex
+                i++
+                logger.warn(ex) { "Database query error Loop $i message: ${ex.message}" }
+                runBlocking { delay(10000) }
+            }
+        }
+    }
+
+    private fun getSugar(pcuCode: String, visitNumber: Int): Double? {
         return jdbiDao.instant.withHandle<List<Double>, Exception> { handle ->
             handle.createQuery(query)
                 .bind("pcucode", pcuCode)
