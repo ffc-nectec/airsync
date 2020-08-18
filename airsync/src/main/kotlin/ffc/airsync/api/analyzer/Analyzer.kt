090c478c94b3aa3c1ebc827849db14a82c522ff4
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-08-18 13:06:20 +0700

    new Analyzer

--- airsync/src/main/kotlin/ffc/airsync/api/analyzer/Analyzer.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/analyzer/Analyzer.kt
@@ -20,6 +20,7 @@
 package ffc.airsync.api.analyzer
 
 import ffc.airsync.analyzerSyncApi
+import ffc.airsync.api.analyzer.lib.NewProcessAnalyzer
 import ffc.airsync.utils.load
 import ffc.airsync.utils.save
 import ffc.entity.healthcare.HealthCareService
@@ -34,24 +35,13 @@ fun HashMap<String, HealthAnalyzer>.initSync(
     localAnalyzer.putAll(localAnalyzer.load("analyzer.json"))
 
     if (localAnalyzer.isEmpty()) {
-        val processSet = HashSet<String>()
-        healthCareService.forEach {
-            processSet.add(it.patientId)
-        }
-        progressCallback(10)
-        val sizeOfLoop = processSet.size
-
-        processSet.forEachIndexed { index, patientId ->
-            val analyzer = HealthAnalyzer()
-            val visit = healthCareService.filter { it.patientId == patientId }
-            analyzer.analyze(*visit.toTypedArray())
-            localAnalyzer[patientId] = analyzer
-            if (sizeOfLoop != 0)
-                progressCallback(((index * 40) / sizeOfLoop) + 10)
-        }
-
         val processCloud = hashMapOf<String, HealthAnalyzer>()
-        processCloud.putAll(analyzerSyncApi.insert(localAnalyzer, progressCallback))
+        processCloud.putAll(
+            analyzerSyncApi.insert(
+                NewProcessAnalyzer().analyzerGroup(healthCareService),
+                progressCallback
+            )
+        )
 
         localAnalyzer.clear()
         localAnalyzer.putAll(processCloud)
---
+++ airsync/src/main/kotlin/ffc/airsync/api/analyzer/lib/NewProcessAnalyzer.kt
@@ -0,0 +1,81 @@
+/*
+ *
+ *  * Copyright 2020 NECTEC
+ *  *   National Electronics and Computer Technology Center, Thailand
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package ffc.airsync.api.analyzer.lib
+
+import ffc.airsync.api.analyzer.lib.NewAnalyzer.Tag.OK
+import ffc.airsync.api.analyzer.lib.NewAnalyzer.Tag.StickBed
+import ffc.airsync.api.analyzer.lib.NewAnalyzer.Tag.StickHouse
+import ffc.entity.healthcare.HealthCareService
+import ffc.entity.healthcare.Severity.LOW
+import ffc.entity.healthcare.Severity.MID
+import ffc.entity.healthcare.analyze.HealthAnalyzer
+import ffc.entity.healthcare.analyze.HealthIssue.Issue.ACTIVITIES
+import ffc.entity.healthcare.analyze.HealthProblem
+
+class NewProcessAnalyzer : NewAnalyzer {
+    override fun analyzer(healthCareServices: List<HealthCareService>): HealthAnalyzer {
+        val analyzer = HealthAnalyzer()
+        analyzer.analyze(*healthCareServices.toTypedArray())
+        return analyzer
+    }
+
+    override fun analyzerGroup(
+        healthCareServices: List<HealthCareService>
+    ): Map<String, HealthAnalyzer> {
+        // สร้าง List รายการคนสำหรับประมวลผล
+        val processList = createProcessList(healthCareServices)
+
+        return processList.map { item ->
+            item.key to analyzer(item.value)
+        }.toMap()
+    }
+
+    private fun createProcessList(healthCareServices: List<HealthCareService>): HashMap<String, ArrayList<HealthCareService>> {
+        val processList = hashMapOf<String, ArrayList<HealthCareService>>()
+        healthCareServices.forEach {
+            if (processList[it.patientId] == null) processList[it.patientId] = arrayListOf()
+            processList[it.patientId]!!.add(it)
+        }
+        return processList
+    }
+
+    override fun analyzerGroupAutoAddTag(
+        healthCareServices: List<HealthCareService>,
+        addTag: () -> NewAnalyzer.AddTag
+    ): Map<String, HealthAnalyzer> {
+        // สร้าง List รายการคนสำหรับประมวลผล
+        val processList = createProcessList(healthCareServices)
+
+        return processList.map { item ->
+            val analyzer = analyzer(item.value)
+
+            // หาผู้สูงอายุติดบ้าน ติดเตียง
+            if (addTag().getAge(item.key) >= 60)
+                analyzer.result[ACTIVITIES]?.let {
+                    when ((it as HealthProblem).severity) {
+                        MID -> addTag().addTag(item.key, StickHouse)
+                        LOW -> addTag().addTag(item.key, StickBed)
+                        else -> addTag().addTag(item.key, OK)
+                    }
+                }
+            item.key to analyzer
+        }.toMap()
+    }
+}
