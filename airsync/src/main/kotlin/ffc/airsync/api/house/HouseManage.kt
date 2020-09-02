46e65214ad38ac9fcd3e950fd8622ef71049947f
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-09-02 15:26:18 +0700

    เพิ่ม Housemange - จะมาใช้แทน การเข้าถึง dao โดยตรงหมด - ออกแบบโครงสร้าง func ใหม่ เพื่อให้สามารถ ไมเกรดได้ง่ายขึ้น

---
+++ airsync/src/main/kotlin/ffc/airsync/api/house/HouseManage.kt
@@ -0,0 +1,109 @@
+/*
+ *
+ *  * Copyright 2020 NECTEC
+ *  *   National Electronics and Computer Technology Center, Thailand
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package ffc.airsync.api.house
+
+import ffc.airsync.Main
+import ffc.airsync.api.sync.ProSync
+import ffc.airsync.api.sync.V1ProSync
+import ffc.airsync.db.DatabaseDao
+import ffc.airsync.utils.`อัพเดทไปยัง`
+import ffc.airsync.utils.ffcFileLoad
+import ffc.airsync.utils.ffcFileSave
+import ffc.entity.Entity
+import ffc.entity.Village
+import ffc.entity.copy
+import ffc.entity.place.House
+import java.io.File
+
+class HouseManage(
+    val dao: DatabaseDao = Main.instant.dao,
+    val houseApi: HouseApi = HouseServiceApi(),
+    private val func: () -> Func
+) : HouseInterface {
+    private var cloudCache = arrayListOf<House>()
+    private val file = File("data", "house.json")
+
+    init {
+
+        cloudCache.addAll(ffcFileLoad<House>(file))
+    }
+
+    interface Func {
+        fun villageLookup(villageCode: String): Village?
+        fun chronicInHouse(pcuCode: String, hcode: String): Boolean
+    }
+
+    override val local: List<House>
+        get() = dao.getHouse(lookupVillage = { func().villageLookup(it) }).map {
+            val hcode = getHcode(it)
+            val pcuCode = getPcuCode(it)
+            it.haveChronic = func().chronicInHouse(pcuCode, hcode)
+            it
+        }
+
+    private fun getPcuCode(it: House) = it.link!!.keys["pcucode"] as String
+
+    private fun getHcode(it: House) = it.link!!.keys["hcode"] as String
+
+    override val cloud: List<House> = cloudCache.toList()
+
+    override fun sync(force: Boolean): List<Entity>? {
+        val proSync: ProSync<House> = V1ProSync()
+        run {
+            val listUpdate = arrayListOf<House>()
+            proSync.update(local, cloudCache) { house ->
+                object : ProSync.UpdateFunc<House> {
+                    override val unixTime: Long = house.timestamp.millis
+                    override val identity: String = house.getIdentity()
+                    override fun updateTo(item: House) {
+                        listUpdate.add(house.copy(item.id))
+                    }
+                }
+            }
+            if (listUpdate.isNotEmpty()) {
+                synchronized(file) {
+                    houseApi.update(listUpdate) `อัพเดทไปยัง` cloudCache
+                    ffcFileSave(file, cloudCache)
+                }
+            }
+        }
+        run {
+            val listCreateDataToCloud = arrayListOf<House>()
+            proSync.createNewDataInB(local, cloudCache) { house ->
+                object : ProSync.CreateFunc<House> {
+                    override val identity: String = house.getIdentity()
+                    override val bIsDelete: Boolean = false
+                    override fun createInB() {
+                        listCreateDataToCloud.add(house)
+                    }
+                }
+            }
+            if (listCreateDataToCloud.isNotEmpty()) {
+                synchronized(file) {
+                    houseApi.createHouse(listCreateDataToCloud, {}, false) `อัพเดทไปยัง` cloudCache
+                    ffcFileSave(file, cloudCache)
+                }
+            }
+        }
+        return cloudCache.toList()
+    }
+
+    private fun House.getIdentity(): String = "${getPcuCode(this)}:${this.no}"
+}
