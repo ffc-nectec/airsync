dced2c11d8d669149735e01b94729deae87e6860
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-09-04 23:35:16 +0700

    ปรับปรุง Performance - ไม่เอาคนนอกเขต - ไม่เอา visit คนนอกเขต - เวลา Lookup icd10 แล้ว 404 ให้แคชไว้ด้วยจะได้ไม่ต้องดึงซ้ำ

--- airsync/src/main/kotlin/ffc/airsync/api/icd10/Icd10ServiceApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/icd10/Icd10ServiceApi.kt
@@ -26,14 +26,27 @@ import ffc.entity.healthcare.Icd10
 
 class Icd10ServiceApi : RetofitApi<Icd10Service>(Icd10Service::class.java, 10240), Icd10Api {
     private val logger by lazy { getLogger(this) }
+    private val cacheIcd404 = HashSet<String>()
+    private var count404 = 0
     override fun lookup(icd10: String): Icd10 {
 
         val icd10Upper = icd10.toUpperCase()
         return callApi {
+            if (cacheIcd404.contains(icd10Upper)) {
+                count404++
+                printIcd10NotFoundLog(icd10Upper)
+                return@callApi Icd10(icd10 = icd10Upper, id = icd10Upper, name = "")
+            }
+
             val response = restService.lookupIcd10(
                 authkey = tokenBarer,
                 id = icd10Upper
             ).execute()
+            if (response.code() == 404) {
+                count404++
+                cacheIcd404.add(icd10Upper)
+                printIcd10NotFoundLog(icd10Upper)
+            }
             if (response.code() != 200) {
                 val errorBody = response.errorBody()?.byteStream()?.reader()?.readLines()
                 val message = "Error LookupICD10=$icd10Upper error=${response.code()} body=$errorBody"
@@ -41,5 +54,9 @@ class Icd10ServiceApi : RetofitApi<Icd10Service>(Icd10Service::class.java, 10240
             }
             response.body() ?: Icd10(icd10 = icd10Upper, id = icd10Upper, name = "")
         }
+    }
+
+    private fun printIcd10NotFoundLog(icd10Upper: String) {
+        logger.warn { "icd10 $icd10Upper is 404. Count all 404 = $count404" }
     }
 }
