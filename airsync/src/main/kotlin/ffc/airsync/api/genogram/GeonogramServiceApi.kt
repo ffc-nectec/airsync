bd62f7de63ce246722f7b8f9e48891e121b68fe5
Max Thanachai Thongkum <singmax161@gmail.com>
2020-10-12 15:58:27 +0700

    fixbug-genogram (#50)

--- airsync/src/main/kotlin/ffc/airsync/api/genogram/GeonogramServiceApi.kt
+++ airsync/src/main/kotlin/ffc/airsync/api/genogram/GeonogramServiceApi.kt
@@ -23,7 +23,6 @@ import ffc.airsync.retrofit.RetofitApi
 import ffc.airsync.utils.ApiLoopException
 import ffc.airsync.utils.UploadSpliterMap
 import ffc.airsync.utils.callApi
-import ffc.airsync.utils.callApiNoReturn
 import ffc.airsync.utils.getLogger
 import ffc.entity.Person
 
@@ -62,14 +61,11 @@ class GeonogramServiceApi : RetofitApi<GenogramService>(GenogramService::class.j
         progressCallback: (Int) -> Unit
     ): Map<String, List<Person.Relationship>> {
         val output = hashMapOf<String, List<Person.Relationship>>()
-        callApiNoReturn { restService.cleanAll(organization.id, tokenBarer).execute() }
         val fixSizeCake = 100
         val sizeOfLoop = relationship.size / fixSizeCake
         UploadSpliterMap.upload(fixSizeCake, relationship) { list, block ->
 
             val result = callApi {
-                restService.unConfirmBlock(organization.id, tokenBarer, block).execute()
-
                 val response = restService.insertBlock(
                     organization.id, tokenBarer,
                     block = block,
@@ -77,10 +73,6 @@ class GeonogramServiceApi : RetofitApi<GenogramService>(GenogramService::class.j
                 ).execute()
 
                 if (response.code() == 201 || response.code() == 200) {
-                    restService.confirmBlock(
-                        organization.id, tokenBarer,
-                        block = block
-                    )
                     response.body()
                 } else {
                     throw ApiLoopException("Response code wrong.")
